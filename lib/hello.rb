d654dd2960aeffcfba81b0a372a46f48e8fde9d9
Sarah Paz <sarah.paz@gmail.com>
2018-12-20 14:46:55 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,8 +1,8 @@
 def hello_t(array)
-  
+
   if block_given?
     i = 0
-    
+
     while i < array.length
       yield array[i]
       i += 1
@@ -14,3 +14,4 @@ def hello_t(array)
 end
 
 # call your method here!
+hello_t(["Tim", "Tom", "Jim"])
